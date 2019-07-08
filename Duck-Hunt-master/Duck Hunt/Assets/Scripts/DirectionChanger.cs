86c0e62ae1071623eb628bb38d03d4ca1cbe0f6b
VIVI <z1125411235@gmail.com>
2019-07-09 02:58:44 +0800

    新增 Duck Hunt重置版 檔案

---
+++ Duck-Hunt-master/Duck Hunt/Assets/Scripts/DirectionChanger.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DirectionChanger: MonoBehaviour
+{
+	public enum Changer {Horizontal, Vertical};
+	public Changer direction;
+
+	void Start ()
+	{
+		GameManager.OnDuckShot += TurnOff;
+		GameManager.OnDuckMiss += TurnOff;
+		GameManager.OnSpawnDucks += TurnOn;
+	}
+	
+	//void Update () {}
+
+	public void OnCollisionEnter(Collision hit)
+	{
+		if (hit.transform.tag == "Duck")
+		{
+			DuckMovement movement = hit.transform.GetComponent<DuckMovement>();
+
+			if (direction == Changer.Horizontal)
+			{
+				movement.DirectionChanger(new Vector3(-1f, 1, 0));
+			}
+			else if (direction == Changer.Vertical)
+			{
+				movement.DirectionChanger(new Vector3(1f, -1f, 0));
+			}
+		}
+	}
+
+	public void TurnOff()
+	{
+		gameObject.SetActive (false);
+	}
+
+	public void TurnOn()
+	{
+		gameObject.SetActive (true);
+	}
+}
---
+++ Duck-Hunt-master/Duck Hunt/Assets/Scripts/DogLayerSwitch.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DogLayerSwitch : MonoBehaviour
+{
+    public int sortingOrder = 0;
+    private SpriteRenderer sprite;
+
+    //void Start () {}
+
+    //void Update () {}
+
+    void SwitchLayerBack()
+	{
+        sprite = GetComponent<SpriteRenderer>();
+        sprite.sortingLayerName = "Dog";
+    }
+
+    void SwitchLayerFront()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        sprite.sortingLayerName = "Foreground";
+    }
+}
